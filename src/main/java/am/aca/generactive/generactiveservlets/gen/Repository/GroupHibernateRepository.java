71eb199cc023566648ebd099239dac479c283ff5
mouradianelen <elen_muradyan@edu.aua.am>
2021-09-25 18:36:25 +0400

    "hibernate updates"

--- src/main/java/am/aca/generactive/generactiveservlets/gen/Repository/GroupHibernateRepository.java
+++ src/main/java/am/aca/generactive/generactiveservlets/gen/Repository/GroupHibernateRepository.java
@@ -1,31 +1,75 @@
 package am.aca.generactive.generactiveservlets.gen.Repository;
 
 import am.aca.generactive.generactiveservlets.gen.model.Group;
+import am.aca.generactive.generactiveservlets.gen.model.Item;
 import am.aca.generactive.generactiveservlets.gen.orm.HibernateConfiguration;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public class GroupHibernateRepository {
     public static final HibernateConfiguration HIBERNATE_CONFIGURATION =
             HibernateConfiguration.getInstance();
 
+    public Group insert(Group group) {
+        Session session = HIBERNATE_CONFIGURATION.getSession();
+        Transaction transaction = session.beginTransaction();
+
+
+        session.save(group);
+
+        transaction.commit();
+
+        session.close();
+
+        return group;
+    }
+
+    public Group update(Group group) {
+        Session session = HIBERNATE_CONFIGURATION.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        Group existing = session.get(Group.class, group.getId());
+        existing.setName(group.getName());
+        existing.setItems(group.getItems());
+
+        transaction.commit();
+
+        session.close();
+
+        return existing;
+    }
+
+    public List<? extends Group> getAllGroups() {
+        Session session = HIBERNATE_CONFIGURATION.getSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        String q = "from Group g";
+        javax.persistence.Query query = session.createQuery(q, Group.class);
+
+        List<? extends Group> groups = query.getResultList();
+
+        transaction.commit();
+        session.close();
+
+        return groups;
+    }
+
     public Optional<Group> getGroup(long groupId) {
         Session session = HIBERNATE_CONFIGURATION.getSession();
         Transaction transaction = session.beginTransaction();
 
-        // Hibernate query to get group by id
-        // equivalent sql query: select * from group where id = ?;
-        // equivalent JPQL query: select i from Group i where id = :id;
-        // Group is the name of the Entity defined by @Entity annotation
+
         Query<Group> query = session.createQuery(
                 "select g from Group g" +
                         " where g.id = :id ",
                 Group.class);
         query.setParameter("id", groupId);
-        // Execute the query and get single result
+
         Group group = query.getSingleResult();
 
         transaction.commit();
--- src/main/java/am/aca/generactive/generactiveservlets/servlets/ItemsServlet.java
+++ src/main/java/am/aca/generactive/generactiveservlets/servlets/ItemsServlet.java
@@ -2,30 +2,23 @@ package am.aca.generactive.generactiveservlets.servlets;
 
 import am.aca.generactive.generactiveservlets.gen.Repository.ItemHibernateRepository;
 import am.aca.generactive.generactiveservlets.gen.Repository.ItemRepository;
-import am.aca.generactive.generactiveservlets.gen.jdbc_util.JDBCutil;
 import am.aca.generactive.generactiveservlets.gen.model.GenerativeItem;
 import am.aca.generactive.generactiveservlets.gen.model.Item;
-import am.aca.generactive.generactiveservlets.gen.model.StockItem;
-import am.aca.generactive.generactiveservlets.gen.util.IdGenerator;
-import am.aca.generactive.generactiveservlets.gen.util.Type;
 import am.aca.generactive.generactiveservlets.servlets.enums.ItemType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jdk.nashorn.internal.scripts.JD;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
+
 import java.util.stream.Collectors;
 
 import static am.aca.generactive.generactiveservlets.servlets.HttpConstants.CONTENT_TYPE_JSON;
 
-@WebServlet(name = "ItemsServlet", value = "/item/*")
+@WebServlet(name = "ItemServlet", value = "/item/*")
 public class ItemsServlet extends HttpServlet {
 
     public static final String PARAM_TYPE = "type";
@@ -38,7 +31,7 @@ public class ItemsServlet extends HttpServlet {
      * Respond with created {@link Item} and status code {@code 200}.
      * Respond with error message and status code {@code 400}, if missing required
      * param(s), wrong {@link ItemType} string representation.
-     *
+     * <p>
      * Create Item will be saved in {@link ItemRepository}
      */
     @Override
